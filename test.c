a8869c44988a713518e250a4a911a1d404c66252
Thomas Munro <thomas.munro@gmail.com>
2024-12-07 18:52:50 +1300

    Test if _configthreadlocale() works.

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <locale.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	printf("does _configthreadlocale() work? ... ");
+	printf(_configthreadlocale(_ENABLE_PER_THREAD_LOCALE) == -1 ? "no\n" : "yes\n");
+
+    return 0;
+}
